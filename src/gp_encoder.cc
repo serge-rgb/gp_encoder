1448350c49c605269fd485dfa61c6c676f24c27e
Sergio Gonzalez <bigmonachus@gmail.com>
2014-12-02 15:15:20 -0600

    Start implementing JPEG header

--- src/gp_encoder.cc
+++ src/gp_encoder.cc
@@ -1,8 +1,17 @@
 /**
- *
  * gp_encoder.cc
  *
+ * Sergio's Tiny Encoder.
+ *
  * 2014 Sergio Gonzalez
+ *
+ * [WIP] Implements JPEG.
+ *
+ * Supports:
+ *  Windows, MSVC
+ *
+ * Does not support:
+ *  Big endian processors.
  *
  */
 
@@ -18,13 +27,10 @@
 #include "../third_party/stb/stb_image.h"
 
 ///////////////////////////////////////////////////////////////////////////////
-// Language macros
+// Language defines
 ///////////////////////////////////////////////////////////////////////////////
 
-#define internal_variable   static
-#define internal_constant   static const
-#define internal            static
-#define local_persist       static
+// ............
 
 ///////////////////////////////////////////////////////////////////////////////
 // Define types
@@ -44,9 +50,69 @@ typedef int32         bool32;
 // Code
 ///////////////////////////////////////////////////////////////////////////////
 
-// SOI      ffd8
-// APP0     ffe0
+// JFIF image header:
+// SOI(ffd8)
+// APP0 [JFIF FRAME]
+// COM | len | "blah"
+// DQT <p.39>               // TODO: Why more than once?
+// SOF <p.35>
+// DHT <p.40>
 //
+
+static uint8 k_jfif_id[] = "JFIF";
+static uint8 k_com_str[] = "Created by Sergio's Tiny Encoder";
+#pragma pack(1)
+typedef struct s_JPEGHeader
+{
+    uint16 SOI;
+    // JFIF header.
+    uint16 APP0;
+    uint16 jfif_len;
+    uint8  jfif_id[5];
+    uint16 version;
+    uint8  units;
+    uint16 x_density;
+    uint16 y_density;
+    uint16  thumb_size;
+
+    // Our signature
+    uint16 com;
+    uint16 com_len;
+    char com_str[sizeof(k_com_str) - 1];
+} JPEGHeader;
+#pragma pack(pop)
+
+// Memory order as big endian. 0xhilo -> 0xlohi which looks as 0xhilo in memory.
+static uint16 be_word(uint16 le_word)
+{
+    uint8 lo = (uint8)(le_word & 0x00ff);
+    uint8 hi = (uint8)((le_word & 0xff00) >> 8);
+    return (((uint16)lo) << 8) | hi;
+}
+
+static JPEGHeader gen_jpeg_header()
+{
+    JPEGHeader header;
+    header.SOI = be_word(0xffd8);  // Sequential DCT
+
+    // JFIF header.
+    header.APP0 = be_word(0xffe0);
+
+    header.jfif_len = be_word(16);
+    memcpy(header.jfif_id, k_jfif_id, 5);
+    header.version = be_word(0x0102);
+    header.units = 0x01;
+    header.x_density = be_word(0x0060);  // 96 DPI
+    header.y_density = be_word(0x0060);  // 96 DPI
+    header.thumb_size = 0;
+
+    // Our signature
+    header.com = be_word(0xfffe);
+    header.com_len = be_word(sizeof(k_com_str) - 1);
+    memcpy(header.com_str, k_com_str, sizeof(k_com_str) - 1);
+
+    return header;
+}
 
 int CALLBACK WinMain(
         HINSTANCE hInstance,
@@ -54,16 +120,29 @@ int CALLBACK WinMain(
         LPSTR lpCmdLine,
         int nCmdShow)
 {
+
+    // Make an endianness check. We only support little endian.
+    {
+        //    0xaabb
+        // -> 0xbbaa (memory)
+        //         ^--- pointer
+        uint16 foo = 0xaabb;
+        char* pointer = (char*)&foo;
+        if (*pointer == 0xaa) {
+            OutputDebugStringA("This machine is big endian. Not supported.");
+            return 1;
+        }
+    }
     int width;
     int height;
     int num_components;
 
-    uint8* data = stbi_load("../in.bmp", &width, &height, &num_components, 3);
+    uint8* data = stbi_load("../in.bmp", &width, &height, &num_components, 0);
 
     if (!data)
     {
         OutputDebugStringA("Could not load bmp file.\n");
-        goto exit;
+        return 1;
     }
 
     assert (num_components == 3);
@@ -71,8 +150,17 @@ int CALLBACK WinMain(
     // TODO: support arbitrary resolutions.
     if (((height % 8) != 0) || ((width % 8) != 0))
     {
-        OutputDebugStringA("Supported resolutions are width and height multiples of 8.");
-        goto exit;
+        OutputDebugStringA(
+                "Supported resolutions are width and height multiples of 8.");
+        return 1;
+    }
+
+    FILE* file_out = fopen("../out.jpg", "wb");
+    if (!file_out)
+    {
+        OutputDebugStringA(
+                "Could not open file for writing.");
+        return 1;
     }
 
     for (int y = 0; y < height; ++y)
@@ -92,12 +180,29 @@ int CALLBACK WinMain(
         }
     }
 
-    char buffer[256];
-    sprintf(buffer, "Image size is  %dx%d\nComponents: %d\n", width, height, num_components);
-    OutputDebugStringA(buffer);
+    // == Write header
+    {
+        JPEGHeader header = gen_jpeg_header();
+        fwrite(&header, sizeof(JPEGHeader), 1, file_out);
+    }
+
+    // Finish the image.
+    {
+        uint16 EOI = be_word(0xffd9);
+        fwrite(&EOI, sizeof(uint16), 1, file_out);
+    }
+    fclose(file_out);
+
+    // Try to load the image with stb_image
+    {
+        uint8* my_data =
+            stbi_load("../out.jpg", &width, &height, &num_components, 0);
+        if (!my_data) {
+            OutputDebugStringA("STB could not load my JPEG\n");
+        }
+    }
 
     stbi_image_free(data);
 
-exit:
     return 0;
 }

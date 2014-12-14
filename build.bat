48b5675e4d0e115614defc30e4f6d53c43fa63a2
Sergio Gonzalez <bigmonachus@gmail.com>
2014-12-13 23:01:14 -0600

    add huffman specification tables from spec

--- build.bat
+++ build.bat
@@ -1,6 +1,9 @@
 @echo off
 
+set warnings=/WX /W4 /wd4100 /wd4310
+set defines=/D_CRT_SECURE_NO_WARNINGS /DTJE_STANDALONE /DTJE_DEBUG
+set compiler_flags=/FC /Zi /Od /Oi
 IF NOT EXIST build mkdir build
 pushd build
-cl /nologo /WX /W4 /wd4100 /wd4310 /D_CRT_SECURE_NO_WARNINGS /DTJE_STANDALONE /FC /Zi ..\src\gp_encoder.cc
+cl /nologo /MP %warnings% %defines% %compiler_flags% ..\src\gp_encoder.cc
 popd
